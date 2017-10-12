43cd67479aef28a120122116467d6b02ddd8a930
Robin Bühler <public@openscript.ch>
2017-10-12 13:26:54 +0200

    Add webpack configuration and loaders

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// initialize app
+ReactDOM.render((
+    <div>
+        Hello World
+    </div>
+), document.getElementById('app'));
